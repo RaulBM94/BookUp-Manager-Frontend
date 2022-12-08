c3b2d08a40bb3029a62ec92884fbb835e8f970b6
alvaroacevedo <alvaroacevedo2305@gmail.com>
2022-12-08 10:49:33 +0000

    CREATED: AppBar AppFooter LoginView MailView DELETED: HelloWorld.vue UPDATED: New routes for login, mail and about

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,61 @@
+<template>
+      <v-footer
+        v-bind="localAttrs"
+        :padless="padless"
+      >
+        <v-card
+          flat
+          tile
+          width="100%"
+          class="deep-purple accent-4 text-center"
+        >
+          <v-card-text>
+            <v-btn
+              v-for="(icon, idx) in icons"
+              :key="idx"
+              class="mx-4"
+              icon
+            >
+            <router-link :to="{name: links[idx]}">
+              <v-icon size="24px">
+                {{ icon }}
+              </v-icon>
+            </router-link>
+            </v-btn>
+          </v-card-text>
+  
+          <v-divider></v-divider>
+  
+          <v-card-text class="white--text">
+            {{ new Date().getFullYear() }} — <strong>BookUp Manager</strong>
+          </v-card-text>
+        </v-card>
+      </v-footer>
+</template>
+
+<script>
+export default {
+ name: 'AppFooter',
+    data () {
+        return { 
+            icons: [
+            'mdi-home',
+            'mdi-email',
+            'mdi-login',
+            'mdi-account-supervisor',
+            ],
+            links: [
+                'home',
+                'mail',
+                'login',
+                'about'
+            ]
+        }
+    }
+  
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,16 @@
 <template>
-  <hello-world />
+  <div>
+    
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld.vue'
+
 
   export default {
     name: 'HomeView',
-
     components: {
-      HelloWorld,
-    },
+      
+    }
   }
 </script>
