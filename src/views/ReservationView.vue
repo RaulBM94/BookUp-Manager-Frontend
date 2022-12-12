175005c8ae6c87b3888269c3032871bb64d6e0cd
NickNahuel23 <nicogtp@gmail.com>
2022-12-12 10:54:41 +0000

    reservation in

--- src/views/ReservationView.vue
+++ src/views/ReservationView.vue
@@ -1,28 +1,28 @@
-<template>
+<template class="cap">
     <v-card class="mx-auto my-12" max-width="360">
         <v-card-title>Crear Reserva</v-card-title>
         <v-card-text>
             <v-row allign="center" class="mx-0 mb-3 colour">
-                <v-text-field label="Teléfono" :rules="emailRules" hide-details="auto" filled></v-text-field>
+                <v-text-field label="Teléfono" hide-details="auto" filled>
+            v-model="User.phone"></v-text-field>
             </v-row>
             <v-row allign="center" class="mx-0 mb-3 colour">
-                <v-text-field label="Email" :type="visible ? 'text' : 'password'" hide-details="auto" append-icon="visible ? 'mdi-eye' : 'mdi-eye-off'"
-                    @click:append="visible = !visible" filled></v-text-field>
+                <v-text-field label="Email" hide-details="auto" filled>
+                    v-model="User.email"></v-text-field>
             </v-row>
             <v-row allign="center" class="mx-0 mb-3 colour">
-                <v-text-field label="Hora de reserva" :type="visible ? 'text' : 'password'" hide-details="auto" append-icon="visible ? 'mdi-eye' : 'mdi-eye-off'"
-                    @click:append="visible = !visible" filled></v-text-field>
+                <v-text-field label="Hora de reserva" hide-details="auto" filled>
+                    v-model="User.email"></v-text-field>
             </v-row>
             <v-row allign="center" class="mx-0 mb-3 colour">
-                <v-text-field label="Personas" :type="visible ? 'text' : 'password'" hide-details="auto" append-icon="visible ? 'mdi-eye' : 'mdi-eye-off'"
-                    @click:append="visible = !visible" filled></v-text-field>
+                <v-text-field label="Personas" :rules="emailRules" hide-details="auto" filled></v-text-field>
                 </v-row>
         </v-card-text>
         <v-card-actions>
             <v-row allign="center" class="mx-0 " >
                 <v-btn class="mx-2" dark large color="deep-purple" @click="reserve" elevation="2">
-                Añadir
-            </v-btn>
+                    <v-spacer>Añadir</v-spacer>
+                </v-btn>
             </v-row>
 
         </v-card-actions>
@@ -46,9 +46,6 @@ top: 64px;
 background: #5D26A3;
 }
 
-
-
-/* Purple800 */
 
 
 </style>
