aee30586ae45736db20434654a8ece9c42ade32e
alvaroacevedo <alvaroacevedo2305@gmail.com>
2022-12-12 13:07:49 +0000

    EDITED: HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,585 @@
 <template>
-  <div>
-    
+<div class="frame-41">
+  <div class="rectangle-103"></div>
+
+  <div class="frame-35">
+    <div class="tu-app-de-gesti-n-de-reservas-para-tu-restaurante-">
+      Tu app de gestión de reservas para tu restaurante.
+    </div>
+
+    <div
+      class="-controla-t--mismo-las-reservas-f-cilmente-y-olv-date-de-los-no-show-"
+    >
+      ¡Controla tú mismo las reservas fácilmente y olvídate de los No Show!
+    </div>
+
+    <div class="extended-fab-dark">
+      <div class="state-layer">
+        <div class="label-text">EMPIEZA YA</div>
+      </div>
+    </div>
   </div>
+
+  <div class="rectangle-1032"></div>
+
+  <svg
+    class="icons-mobile_friendly_24px"
+    width="36"
+    height="36"
+    viewBox="0 0 36 36"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M6 31.5V4.5C6 2.85 7.35 1.515 9 1.515L24 1.5C25.65 1.5 27 2.85 27 4.5V10.5H24V9H9V27H24V25.5H27V31.5C27 33.15 25.65 34.5 24 34.5H9C7.35 34.5 6 33.15 6 31.5ZM24 31.5V30H9V31.5H24ZM9 4.5H24V6H9V4.5ZM31.8 14.325L29.7 12.225L22.125 19.8L18.6 16.275L16.5 18.375L22.125 24L31.8 14.325Z"
+      fill="white"
+    />
+  </svg>
+  <svg
+    class="icons-mode_edit_24px"
+    width="36"
+    height="36"
+    viewBox="0 0 36 36"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M28.59 5.38495L30.615 7.40995C31.8 8.57995 31.8 10.4849 30.615 11.6549L10.77 31.4999H4.5V25.2299L20.1 9.61495L24.345 5.38495C25.515 4.21495 27.42 4.21495 28.59 5.38495ZM7.5 28.4999L9.615 28.5899L24.345 13.8449L22.23 11.7299L7.5 26.4599V28.4999Z"
+      fill="white"
+    />
+  </svg>
+  <svg
+    class="icons-access_time_24px"
+    width="36"
+    height="36"
+    viewBox="0 0 36 36"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M17.985 3C9.705 3 3 9.72 3 18C3 26.28 9.705 33 17.985 33C26.28 33 33 26.28 33 18C33 9.72 26.28 3 17.985 3ZM18 30C11.37 30 6 24.63 6 18C6 11.37 11.37 6 18 6C24.63 6 30 11.37 30 18C30 24.63 24.63 30 18 30ZM16.5 19.605L24.735 24.54L26.265 21.96L19.5 17.895V9.63H16.5V19.605Z"
+      fill="white"
+    />
+  </svg>
+
+  <div class="servicios">SERVICIOS</div>
+
+  <div class="gesti-n-de-reservas">
+    <span
+      ><span class="gesti-n-de-reservas-span2"><br /><br /><br /></span
+    ></span>
+  </div>
+
+  <div class="crear-reserva">
+    <span
+      ><span class="crear-reserva-span">Crear reserva<br /></span
+      ><span class="crear-reserva-span2"><br /><br /><br /></span
+    ></span>
+  </div>
+
+  <div class="lista-de-espera">Lista de espera<br /><br /><br /><br /></div>
+
+  <div class="frame-352">
+    <div class="frame-27">
+      <div
+        class="-comienza-a-gestionar-las-reservas-de-tu-restaurante-desde-ya-"
+      >
+        ¡Comienza a gestionar las reservas de tu restaurante desde ya!
+      </div>
+
+      <div class="_5-99----mes">5,99 €/ mes</div>
+
+      <div class="basic">BASIC</div>
+    </div>
+
+    <div class="frame-28">
+      <div
+        class="recoge-todo-el-paquete-basic-con-las--ltimas-funcionalidades-de-nuestro-equipo-de-desarrollo"
+      >
+        Recoge todo el paquete Basic con las últimas funcionalidades de nuestro
+        equipo de desarrollo
+      </div>
+
+      <div class="_7-99---mes">7,99 €/mes</div>
+
+      <div class="premium">PREMIUM</div>
+    </div>
+
+    <div class="frame-29">
+      <div
+        class="accede-a-toda-nuestra-aplicaci-n-sin-l-mites----recursos---crecimiento----"
+      >
+        <span
+          ><span
+            class="accede-a-toda-nuestra-aplicaci-n-sin-l-mites----recursos---crecimiento-----span"
+            >Accede a toda nuestra aplicación sin límites. + Recursos +
+            Crecimiento </span
+          ><span
+            class="accede-a-toda-nuestra-aplicaci-n-sin-l-mites----recursos---crecimiento-----span2"
+            >...</span
+          ></span
+        >
+      </div>
+
+      <div class="_10-99---mes">10,99 €/mes</div>
+
+      <div class="premium--">PREMIUM +</div>
+    </div>
+  </div>
+
+  <div class="rectangle-104"></div>
+
+  <div class="-a-qu--esperas-para-empezar-a-ganar-">
+    ¿A qué esperas para empezar a ganar?
+  </div>
+
+  <div class="extended-fab-dark2">
+    <div class="state-layer">
+      <div class="label-text2">PROBAR DEMO</div>
+    </div>
+  </div>
+  <svg
+    class="vector"
+    width="1918"
+    height="486"
+    viewBox="0 0 1918 486"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M0 0L46.3517 9C91.105 18 182.21 36 273.315 99C366.018 162 457.123 270 548.228 270C639.333 270 730.438 162 821.543 162C912.648 162 1005.35 270 1096.46 315C1187.56 360 1278.67 342 1369.77 342C1460.88 342 1551.98 360 1644.69 324C1735.79 288 1826.9 198 1871.65 153L1918 108V486H1871.65C1826.9 486 1735.79 486 1644.69 486C1551.98 486 1460.88 486 1369.77 486C1278.67 486 1187.56 486 1096.46 486C1005.35 486 912.648 486 821.543 486C730.438 486 639.333 486 548.228 486C457.123 486 366.018 486 273.315 486C182.21 486 91.105 486 46.3517 486H0V0Z"
+      fill="#CDB3FB"
+    />
+  </svg>
+
+  <div class="planes">PLANES</div>
+</div>
+
+
 </template>
 
 <script>
+import {
 
+} from 'vuetify/lib';
 
-  export default {
-    name: 'HomeView',
-    components: {
-      
-    }
-  }
+export default {
+  name: 'HomeView',
+  components: {
+
+  },
+};
 </script>
+
+<style>
+.frame-41,
+.frame-41 * {
+  box-sizing: border-box;
+}
+.frame-41 {
+  background: #ffffff;
+  width: 1920px;
+  height: 1795px;
+  position: relative;
+  overflow: hidden;
+}
+.rectangle-103 {
+  background: linear-gradient(
+    89.72deg,
+    rgba(123, 66, 244, 1) 0%,
+    rgba(114, 71, 130, 0.85) 50%,
+    rgba(206, 89, 246, 1) 100%
+  );
+  border-radius: 0px 0px 420px 0px;
+  width: 1920px;
+  height: 754px;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+}
+.frame-35 {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  align-items: center;
+  justify-content: flex-start;
+  width: 288px;
+  position: absolute;
+  left: 816px;
+  top: 151px;
+}
+.tu-app-de-gesti-n-de-reservas-para-tu-restaurante- {
+  color: var(--purple50, #f8f5ff);
+  text-align: center;
+  font: 600 48px "Roboto", sans-serif;
+  position: relative;
+  width: 420px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.-controla-t--mismo-las-reservas-f-cilmente-y-olv-date-de-los-no-show- {
+  color: var(--purple50, #f8f5ff);
+  text-align: center;
+  font: 400 32px "Roboto", sans-serif;
+  position: relative;
+  width: 731px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.extended-fab-dark {
+  background: var(--purple200, #e3d4fd);
+  border-radius: 16px;
+  display: flex;
+  flex-direction: row;
+  gap: 0px;
+  align-items: center;
+  justify-content: center;
+  flex-shrink: 0;
+  width: 184px;
+  height: 61px;
+  position: relative;
+  box-shadow: var(
+    --m3-elevationdark-3-box-shadow,
+    0px 1px 3px 0px rgba(0, 0, 0, 0.3),
+    0px 4px 8px 3px rgba(0, 0, 0, 0.15)
+  );
+  overflow: hidden;
+}
+.state-layer {
+  padding: 16px 20px 16px 16px;
+  display: flex;
+  flex-direction: row;
+  gap: 12px;
+  align-items: center;
+  justify-content: center;
+  flex-shrink: 0;
+  position: relative;
+}
+.label-text {
+  color: linear-gradient(
+      to left,
+      rgba(103, 80, 164, 0.08),
+      rgba(103, 80, 164, 0.08)
+    ),
+    linear-gradient(to left, #541388, #541388);
+  text-align: center;
+  font: 400 24px "Roboto", sans-serif;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.rectangle-1032 {
+  background: var(--purple400, #b083f8);
+  border-radius: 100px 0px 0px 100px;
+  width: 357px;
+  height: 350px;
+  position: absolute;
+  left: 1563px;
+  top: 346px;
+}
+.rectangle-115 {
+  background: var(--purple300, #cdb3fb);
+  border-radius: 80px 0px 0px 80px;
+  width: 329px;
+  height: 315px;
+  position: absolute;
+  left: 1591px;
+  top: 363px;
+}
+.icons-mobile_friendly_24px {
+  position: absolute;
+  left: 1756px;
+  top: 384px;
+  overflow: visible;
+}
+.icons-mode_edit_24px {
+  position: absolute;
+  left: 1756px;
+  top: 472px;
+  overflow: visible;
+}
+.icons-access_time_24px {
+  position: absolute;
+  left: 1756px;
+  top: 560px;
+  overflow: visible;
+}
+.servicios {
+  color: var(--purple900, #4e2283);
+  text-align: left;
+  font: 600 32px "Roboto", sans-serif;
+  position: absolute;
+  left: 1615.39px;
+  top: 626.25px;
+  transform-origin: 0 0;
+  transform: rotate(-90.378deg) scale(1, 1);
+}
+.gesti-n-de-reservas {
+  color: #ffffff;
+  text-align: center;
+  font: var(--undefined, 400 0px "Roboto", sans-serif);
+  position: absolute;
+  left: 1717px;
+  top: 433px;
+  width: 113px;
+  height: 26px;
+}
+.gesti-n-de-reservas-span {
+  color: #ffffff;
+  font: 400 16px "Roboto", sans-serif;
+}
+.gesti-n-de-reservas-span2 {
+  color: #ffffff;
+  font: var(--roboto14px, 400 14px "Roboto", sans-serif);
+}
+.crear-reserva {
+  color: #ffffff;
+  text-align: center;
+  font: var(--undefined, 400 0px "Roboto", sans-serif);
+  position: absolute;
+  left: 1737px;
+  top: 508px;
+  width: 73px;
+  height: 26px;
+}
+.crear-reserva-span {
+  color: #ffffff;
+  font: 400 16px "Roboto", sans-serif;
+}
+.crear-reserva-span2 {
+  color: #ffffff;
+  font: var(--roboto14px, 400 14px "Roboto", sans-serif);
+}
+.lista-de-espera {
+  color: #ffffff;
+  text-align: center;
+  font: 400 16px "Roboto", sans-serif;
+  position: absolute;
+  left: 1733px;
+  top: 602px;
+  width: 82px;
+  height: 13px;
+}
+.frame-352 {
+  padding: 0px 26px 0px 0px;
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+  align-items: flex-start;
+  justify-content: flex-start;
+  width: 722px;
+  position: absolute;
+  left: 599px;
+  top: 796px;
+  overflow: hidden;
+}
+.frame-27 {
+  background: #470b64;
+  border-radius: 24px;
+  padding: 8px 8px 8px 8px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  align-items: center;
+  justify-content: center;
+  flex-shrink: 0;
+  width: 230px;
+  height: 352px;
+  position: relative;
+}
+.-comienza-a-gestionar-las-reservas-de-tu-restaurante-desde-ya- {
+  color: #ffffff;
+  text-align: left;
+  font: 500 14px "Roboto", sans-serif;
+  position: relative;
+  width: 199px;
+  height: 199px;
+}
+._5-99----mes {
+  color: #ffffff;
+  text-align: left;
+  font: 700 16px "Inter", sans-serif;
+  position: relative;
+  width: 99px;
+  height: 13px;
+}
+.basic {
+  color: #ffffff;
+  text-align: left;
+  font: 700 18px "Inter", sans-serif;
+  position: absolute;
+  left: calc(50% - 28px);
+  top: 28px;
+  width: 56px;
+}
+.frame-28 {
+  background: #470b64;
+  border-radius: 24px;
+  padding: 8px 8px 8px 8px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  align-items: center;
+  justify-content: center;
+  flex-shrink: 0;
+  width: 230px;
+  height: 352px;
+  position: relative;
+}
+.recoge-todo-el-paquete-basic-con-las--ltimas-funcionalidades-de-nuestro-equipo-de-desarrollo {
+  color: #ffffff;
+  text-align: left;
+  font: 500 14px "Roboto", sans-serif;
+  position: relative;
+  width: 199px;
+  height: 199px;
+}
+._7-99---mes {
+  color: #ffffff;
+  text-align: left;
+  font: 700 16px "Inter", sans-serif;
+  position: relative;
+  width: 94px;
+  height: 13px;
+}
+.premium {
+  color: #ffffff;
+  text-align: left;
+  font: 700 18px "Inter", sans-serif;
+  position: absolute;
+  left: 72px;
+  top: 28px;
+}
+.frame-29 {
+  background: #470b64;
+  border-radius: 24px;
+  padding: 8px 8px 8px 8px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  align-items: center;
+  justify-content: center;
+  flex-shrink: 0;
+  width: 230px;
+  height: 352px;
+  position: relative;
+}
+.accede-a-toda-nuestra-aplicaci-n-sin-l-mites----recursos---crecimiento---- {
+  text-align: left;
+  font: 500 14px "Inter", sans-serif;
+  position: relative;
+  width: 199px;
+  height: 199px;
+}
+.accede-a-toda-nuestra-aplicaci-n-sin-l-mites----recursos---crecimiento-----span {
+  color: #ffffff;
+  font: 500 14px "Inter", sans-serif;
+}
+.accede-a-toda-nuestra-aplicaci-n-sin-l-mites----recursos---crecimiento-----span2 {
+  color: #470b64;
+  font: 500 14px "Inter", sans-serif;
+}
+._10-99---mes {
+  color: #ffffff;
+  text-align: left;
+  font: 700 16px "Inter", sans-serif;
+  position: relative;
+  width: 101px;
+  height: 13px;
+}
+.premium-- {
+  color: #ffffff;
+  text-align: left;
+  font: 700 18px "Inter", sans-serif;
+  position: absolute;
+  left: calc(50% - 51px);
+  top: 28px;
+}
+.rectangle-104 {
+  background: var(--purple400, #b083f8);
+  border-radius: 100px 0px 0px 100px;
+  width: 755px;
+  height: 276px;
+  position: absolute;
+  left: 755px;
+  top: 1465px;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  transform-origin: 0 0;
+  transform: rotate(-180deg) scale(1, 1);
+}
+.-a-qu--esperas-para-empezar-a-ganar- {
+  color: var(--purple900, #4e2283);
+  text-align: center;
+  font: 400 20px "Roboto", sans-serif;
+  position: absolute;
+  left: 264px;
+  top: 1254px;
+  width: 228px;
+  height: 50px;
+}
+.extended-fab-dark2 {
+  background: var(--purple200, #e3d4fd);
+  border-radius: 16px;
+  display: flex;
+  flex-direction: row;
+  gap: 0px;
+  align-items: center;
+  justify-content: center;
+  width: 181px;
+  height: 42px;
+  position: absolute;
+  left: 287px;
+  top: 1315px;
+  box-shadow: var(
+    --m3-elevationdark-3-box-shadow,
+    0px 1px 3px 0px rgba(0, 0, 0, 0.3),
+    0px 4px 8px 3px rgba(0, 0, 0, 0.15)
+  );
+  overflow: hidden;
+}
+.label-text2 {
+  color: linear-gradient(
+      to left,
+      rgba(103, 80, 164, 0.08),
+      rgba(103, 80, 164, 0.08)
+    ),
+    linear-gradient(to left, #541388, #541388);
+  text-align: center;
+  font: 400 20px "Roboto", sans-serif;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.vector {
+  position: absolute;
+  left: 0px;
+  top: 1309px;
+  overflow: visible;
+}
+.planes {
+  color: var(--purple900, #4e2283);
+  text-align: left;
+  font: 700 40px "Roboto", sans-serif;
+  position: absolute;
+  left: 158px;
+  top: 938px;
+}
+
+
+
+</style>
+
+
+
