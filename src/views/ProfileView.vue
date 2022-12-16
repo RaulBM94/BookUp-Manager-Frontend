f2dab9038df8c3e326b2e250dba94aa55999812e
alvaroacevedo <alvaroacevedo2305@gmail.com>
2022-12-16 17:39:11 +0000

    files merged

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,573 @@
+<template>
+  <div class="desktop">
+    <svg
+      class="vector"
+      width="1920"
+      height="349"
+      viewBox="0 0 1920 349"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M0 58L29 77C58 96 116 134 175 163C233 192 291 211 349 230C407 250 465 269 524 288C582 307 640 326 698 307C756 288 815 230 873 182C931 134 989 96 1047 144C1105 192 1164 326 1222 346C1280 365 1338 269 1396 211C1455 154 1513 134 1571 173C1629 211 1687 307 1745 336C1804 365 1862 326 1891 307L1920 288V0H1891C1862 0 1804 0 1745 0C1687 0 1629 0 1571 0C1513 0 1455 0 1396 0C1338 0 1280 0 1222 0C1164 0 1105 0 1047 0C989 0 931 0 873 0C815 0 756 0 698 0C640 0 582 0 524 0C465 0 407 0 349 0C291 0 233 0 175 0C116 0 58 0 29 0H0V58Z"
+        fill="#5D26A3"
+      />
+    </svg>
+
+    <div class="group-2608562">
+      <div class="rectangle-98"></div>
+
+      <div class="mis-datos">Mis Datos</div>
+    </div>
+
+    <div class="small-fab">
+      <div class="antonio-garc-a">Antonio García</div>
+    </div>
+
+    <div class="small-fab2">
+      <div class="contrase-a">Contraseña</div>
+    </div>
+
+    <div class="small-fab3">
+      <div class="repetir-contrase-a">Repetir Contraseña</div>
+    </div>
+
+    <div class="small-fab4">
+      <div class="antoniogarcia1-gmail-com">antoniogarcia1@gmail.com</div>
+    </div>
+
+    <div class="small-fab5">
+      <div class="-34-123-456-789">+34 123 456 789</div>
+    </div>
+
+    <div class="small-fab6">
+      <svg
+        class="icon"
+        width="32"
+        height="33"
+        viewBox="0 0 32 33"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M16.0003 3.1665C8.64033 3.1665 2.66699 9.13984 2.66699 16.4998C2.66699 23.8598 8.64033 29.8332 16.0003 29.8332C23.3603 29.8332 29.3337 23.8598 29.3337 16.4998C29.3337 9.13984 23.3603 3.1665 16.0003 3.1665ZM24.4803 22.9398C22.5737 20.6198 17.947 19.8332 16.0003 19.8332C14.0537 19.8332 9.42699 20.6198 7.52033 22.9398C6.16033 21.1532 5.33366 18.9265 5.33366 16.4998C5.33366 10.6198 10.1203 5.83317 16.0003 5.83317C21.8803 5.83317 26.667 10.6198 26.667 16.4998C26.667 18.9265 25.8403 21.1532 24.4803 22.9398ZM11.3337 13.1665C11.3337 10.5798 13.4137 8.49984 16.0003 8.49984C18.587 8.49984 20.667 10.5798 20.667 13.1665C20.667 15.7532 18.587 17.8332 16.0003 17.8332C13.4137 17.8332 11.3337 15.7532 11.3337 13.1665Z"
+          fill="#6750A4"
+        />
+      </svg>
+
+      <div class="foto-de-perfil">Foto de perfil</div>
+    </div>
+
+    <div class="small-fab7">
+      <div class="grill-garcia-s">Grill Garcia’s</div>
+    </div>
+
+    <div class="small-fab8">
+      <div class="horario">Horario</div>
+    </div>
+
+    <div class="small-fab9">
+      <div class="calle-juncos--s-n">Calle Juncos, S/N</div>
+    </div>
+
+    <div class="small-fab10">
+      <div class="n-mero-de-mesas">Número de mesas</div>
+    </div>
+
+    <div class="group-2608565">
+      <div class="rectangle-982"></div>
+
+      <div class="datos-del-establecimiento">Datos del establecimiento</div>
+    </div>
+
+    <div class="extended-fab-dark">
+      <div class="state-layer">
+        <div class="label-text">EDITAR</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+
+export default {
+};
+</script>
+<style scoped>
+.desktop,
+.desktop * {
+  box-sizing: border-box;
+}
+.desktop {
+  background: #ffffff;
+  width: 1920px;
+  height: 928px;
+  position: relative;
+  overflow: hidden;
+}
+.vector {
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  overflow: visible;
+}
+.group-2608562 {
+  width: 622px;
+  height: 35px;
+  position: static;
+}
+.rectangle-98 {
+  background: var(--purple400, #b083f8);
+  border-radius: 5px;
+  width: 622px;
+  height: 35px;
+  position: absolute;
+  left: 649px;
+  top: 98px;
+}
+.mis-datos {
+  color: #ffffff;
+  text-align: center;
+  font: 600 24px "Roboto", sans-serif;
+  position: absolute;
+  left: 649px;
+  top: 102px;
+  width: 603px;
+  height: 25px;
+}
+.small-fab {
+  background: var(
+    --m3-surfaces-light-surface3,
+    linear-gradient(
+      to left,
+      rgba(103, 80, 164, 0.11),
+      rgba(103, 80, 164, 0.11)
+    ),
+    linear-gradient(to left, #fffbfe, #fffbfe)
+  );
+  border-radius: 12px;
+  padding: 16px 16px 16px 16px;
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+  align-items: center;
+  justify-content: flex-start;
+  width: 287px;
+  height: 45px;
+  position: absolute;
+  left: 649px;
+  top: 164px;
+  box-shadow: var(
+    --m3-elevationlight-3-box-shadow,
+    0px 1px 3px 0px rgba(0, 0, 0, 0.3),
+    0px 4px 8px 3px rgba(0, 0, 0, 0.15)
+  );
+  overflow: hidden;
+}
+.antonio-garc-a {
+  color: #000000;
+  text-align: center;
+  font: var(--roboto14px, 400 14px "Roboto", sans-serif);
+  position: relative;
+  width: 249px;
+}
+.small-fab2 {
+  background: var(
+    --m3-surfaces-light-surface3,
+    linear-gradient(
+      to left,
+      rgba(103, 80, 164, 0.11),
+      rgba(103, 80, 164, 0.11)
+    ),
+    linear-gradient(to left, #fffbfe, #fffbfe)
+  );
+  border-radius: 12px;
+  padding: 16px 16px 16px 16px;
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+  align-items: center;
+  justify-content: flex-start;
+  width: 287px;
+  height: 45px;
+  position: absolute;
+  left: 984px;
+  top: 240px;
+  box-shadow: var(
+    --m3-elevationlight-3-box-shadow,
+    0px 1px 3px 0px rgba(0, 0, 0, 0.3),
+    0px 4px 8px 3px rgba(0, 0, 0, 0.15)
+  );
+  overflow: hidden;
+}
+.contrase-a {
+  color: #000000;
+  text-align: center;
+  font: var(--roboto14px, 400 14px "Roboto", sans-serif);
+  position: relative;
+  width: 249px;
+}
+.small-fab3 {
+  background: var(
+    --m3-surfaces-light-surface3,
+    linear-gradient(
+      to left,
+      rgba(103, 80, 164, 0.11),
+      rgba(103, 80, 164, 0.11)
+    ),
+    linear-gradient(to left, #fffbfe, #fffbfe)
+  );
+  border-radius: 12px;
+  padding: 16px 16px 16px 16px;
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+  align-items: center;
+  justify-content: flex-start;
+  width: 287px;
+  height: 45px;
+  position: absolute;
+  left: 984px;
+  top: 316px;
+  box-shadow: var(
+    --m3-elevationlight-3-box-shadow,
+    0px 1px 3px 0px rgba(0, 0, 0, 0.3),
+    0px 4px 8px 3px rgba(0, 0, 0, 0.15)
+  );
+  overflow: hidden;
+}
+.repetir-contrase-a {
+  color: #000000;
+  text-align: center;
+  font: var(--roboto14px, 400 14px "Roboto", sans-serif);
+  position: relative;
+  width: 249px;
+}
+.small-fab4 {
+  background: var(
+    --m3-surfaces-light-surface3,
+    linear-gradient(
+      to left,
+      rgba(103, 80, 164, 0.11),
+      rgba(103, 80, 164, 0.11)
+    ),
+    linear-gradient(to left, #fffbfe, #fffbfe)
+  );
+  border-radius: 12px;
+  padding: 16px 16px 16px 16px;
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+  align-items: center;
+  justify-content: flex-start;
+  width: 287px;
+  height: 45px;
+  position: absolute;
+  left: 984px;
+  top: 164px;
+  box-shadow: var(
+    --m3-elevationlight-3-box-shadow,
+    0px 1px 3px 0px rgba(0, 0, 0, 0.3),
+    0px 4px 8px 3px rgba(0, 0, 0, 0.15)
+  );
+  overflow: hidden;
+}
+.antoniogarcia1-gmail-com {
+  color: #000000;
+  text-align: center;
+  font: var(--roboto14px, 400 14px "Roboto", sans-serif);
+  position: relative;
+  width: 249px;
+}
+.small-fab5 {
+  background: var(
+    --m3-surfaces-light-surface3,
+    linear-gradient(
+      to left,
+      rgba(103, 80, 164, 0.11),
+      rgba(103, 80, 164, 0.11)
+    ),
+    linear-gradient(to left, #fffbfe, #fffbfe)
+  );
+  border-radius: 12px;
+  padding: 16px 16px 16px 16px;
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+  align-items: center;
+  justify-content: flex-start;
+  width: 287px;
+  height: 45px;
+  position: absolute;
+  left: 649px;
+  top: 240px;
+  box-shadow: var(
+    --m3-elevationlight-3-box-shadow,
+    0px 1px 3px 0px rgba(0, 0, 0, 0.3),
+    0px 4px 8px 3px rgba(0, 0, 0, 0.15)
+  );
+  overflow: hidden;
+}
+.-34-123-456-789 {
+  color: #000000;
+  text-align: center;
+  font: var(--roboto14px, 400 14px "Roboto", sans-serif);
+  position: relative;
+  width: 249px;
+}
+.small-fab6 {
+  background: var(
+    --m3-surfaces-light-surface3,
+    linear-gradient(
+      to left,
+      rgba(103, 80, 164, 0.11),
+      rgba(103, 80, 164, 0.11)
+    ),
+    linear-gradient(to left, #fffbfe, #fffbfe)
+  );
+  border-radius: 12px;
+  padding: 16px 16px 16px 60px;
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+  align-items: center;
+  justify-content: flex-start;
+  width: 287px;
+  height: 45px;
+  position: absolute;
+  left: 644px;
+  top: 316px;
+  box-shadow: var(
+    --m3-elevationlight-3-box-shadow,
+    0px 1px 3px 0px rgba(0, 0, 0, 0.3),
+    0px 4px 8px 3px rgba(0, 0, 0, 0.15)
+  );
+  overflow: hidden;
+}
+.icon {
+  flex-shrink: 0;
+  position: relative;
+  overflow: visible;
+}
+.foto-de-perfil {
+  color: #000000;
+  text-align: center;
+  font: var(--roboto14px, 400 14px "Roboto", sans-serif);
+  position: relative;
+  width: 82px;
+}
+.small-fab7 {
+  background: var(
+    --m3-surfaces-light-surface3,
+    linear-gradient(
+      to left,
+      rgba(103, 80, 164, 0.11),
+      rgba(103, 80, 164, 0.11)
+    ),
+    linear-gradient(to left, #fffbfe, #fffbfe)
+  );
+  border-radius: 12px;
+  padding: 16px 16px 16px 16px;
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+  align-items: center;
+  justify-content: flex-start;
+  width: 287px;
+  height: 45px;
+  position: absolute;
+  left: 649px;
+  top: 491px;
+  box-shadow: var(
+    --m3-elevationlight-3-box-shadow,
+    0px 1px 3px 0px rgba(0, 0, 0, 0.3),
+    0px 4px 8px 3px rgba(0, 0, 0, 0.15)
+  );
+  overflow: hidden;
+}
+.grill-garcia-s {
+  color: #000000;
+  text-align: center;
+  font: var(--roboto14px, 400 14px "Roboto", sans-serif);
+  position: relative;
+  width: 249px;
+}
+.small-fab8 {
+  background: var(
+    --m3-surfaces-light-surface3,
+    linear-gradient(
+      to left,
+      rgba(103, 80, 164, 0.11),
+      rgba(103, 80, 164, 0.11)
+    ),
+    linear-gradient(to left, #fffbfe, #fffbfe)
+  );
+  border-radius: 12px;
+  padding: 16px 16px 16px 16px;
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+  align-items: center;
+  justify-content: flex-start;
+  width: 287px;
+  height: 45px;
+  position: absolute;
+  left: 984px;
+  top: 491px;
+  box-shadow: var(
+    --m3-elevationlight-3-box-shadow,
+    0px 1px 3px 0px rgba(0, 0, 0, 0.3),
+    0px 4px 8px 3px rgba(0, 0, 0, 0.15)
+  );
+  overflow: hidden;
+}
+.horario {
+  color: #000000;
+  text-align: center;
+  font: var(--roboto14px, 400 14px "Roboto", sans-serif);
+  position: relative;
+  width: 249px;
+}
+.small-fab9 {
+  background: var(
+    --m3-surfaces-light-surface3,
+    linear-gradient(
+      to left,
+      rgba(103, 80, 164, 0.11),
+      rgba(103, 80, 164, 0.11)
+    ),
+    linear-gradient(to left, #fffbfe, #fffbfe)
+  );
+  border-radius: 12px;
+  padding: 16px 16px 16px 16px;
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+  align-items: center;
+  justify-content: flex-start;
+  width: 287px;
+  height: 45px;
+  position: absolute;
+  left: 649px;
+  top: 562px;
+  box-shadow: var(
+    --m3-elevationlight-3-box-shadow,
+    0px 1px 3px 0px rgba(0, 0, 0, 0.3),
+    0px 4px 8px 3px rgba(0, 0, 0, 0.15)
+  );
+  overflow: hidden;
+}
+.calle-juncos--s-n {
+  color: #000000;
+  text-align: center;
+  font: var(--roboto14px, 400 14px "Roboto", sans-serif);
+  position: relative;
+  width: 249px;
+}
+.small-fab10 {
+  background: var(
+    --m3-surfaces-light-surface3,
+    linear-gradient(
+      to left,
+      rgba(103, 80, 164, 0.11),
+      rgba(103, 80, 164, 0.11)
+    ),
+    linear-gradient(to left, #fffbfe, #fffbfe)
+  );
+  border-radius: 12px;
+  padding: 16px 16px 16px 16px;
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+  align-items: center;
+  justify-content: flex-start;
+  width: 287px;
+  height: 45px;
+  position: absolute;
+  left: 984px;
+  top: 562px;
+  box-shadow: var(
+    --m3-elevationlight-3-box-shadow,
+    0px 1px 3px 0px rgba(0, 0, 0, 0.3),
+    0px 4px 8px 3px rgba(0, 0, 0, 0.15)
+  );
+  overflow: hidden;
+}
+.n-mero-de-mesas {
+  color: #000000;
+  text-align: center;
+  font: var(--roboto14px, 400 14px "Roboto", sans-serif);
+  position: relative;
+  width: 249px;
+}
+.group-2608565 {
+  width: 622px;
+  height: 35px;
+  position: static;
+}
+.rectangle-982 {
+  background: var(--purple400, #b083f8);
+  border-radius: 5px;
+  width: 622px;
+  height: 35px;
+  position: absolute;
+  left: 649px;
+  top: 430px;
+}
+.datos-del-establecimiento {
+  color: #ffffff;
+  text-align: center;
+  font: 500 24px "Roboto", sans-serif;
+  position: absolute;
+  left: 649px;
+  top: 434px;
+  width: 602.56px;
+  height: 22px;
+}
+.extended-fab-dark {
+  background: linear-gradient(
+      to left,
+      rgba(208, 188, 255, 0.11),
+      rgba(208, 188, 255, 0.11)
+    ),
+    linear-gradient(to left, #5200ff, #5200ff);
+  border-radius: 16px;
+  display: flex;
+  flex-direction: row;
+  gap: 0px;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  left: 915px;
+  top: 661px;
+  box-shadow: var(
+    --m3-elevationdark-3-box-shadow,
+    0px 1px 3px 0px rgba(0, 0, 0, 0.3),
+    0px 4px 8px 3px rgba(0, 0, 0, 0.15)
+  );
+  overflow: hidden;
+}
+.state-layer {
+  padding: 16px 20px 16px 16px;
+  display: flex;
+  flex-direction: row;
+  gap: 12px;
+  align-items: center;
+  justify-content: center;
+  flex-shrink: 0;
+  position: relative;
+}
+.label-text {
+  color: var(--m3-sys-dark-primary, #d0bcff);
+  text-align: center;
+  font: var(--roboto14px, 400 14px "Roboto", sans-serif);
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
