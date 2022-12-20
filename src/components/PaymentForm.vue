ecc125cbb61a867fd2628c62d191b213f29ae3c3
RaulBM94 <enderbrumen@gmail.com>
2022-12-20 09:08:12 +0000

    Cambio en payment

--- src/components/PaymentForm.vue
+++ src/components/PaymentForm.vue
@@ -50,7 +50,7 @@ export default {
             },
           ],
           successURL: 'https://bookupmanagerapi.onrender.com/api/success',
-          cancelURL: 'https://bookupmanagerapi.onrender.com/api/personal'
+          cancelURL: 'https://bookupmanagerapi.onrender.com/api/home'
         },
         {
           type: 'Paquete Premium',
@@ -62,7 +62,7 @@ export default {
             },
           ],
           successURL: 'https://bookupmanagerapi.onrender.com/api/success',
-          cancelURL: 'https://bookupmanagerapi.onrender.com/api/personal'
+          cancelURL: 'https://bookupmanagerapi.onrender.com/api/home'
         },
         {
           type: 'Paquete Premium Plus',
@@ -74,7 +74,7 @@ export default {
             },
           ],
           successURL: 'https://bookupmanagerapi.onrender.com/api/success',
-          cancelURL: 'https://bookupmanagerapi.onrender.com/api/personal'
+          cancelURL: 'https://bookupmanagerapi.onrender.com/api/home'
         }
       ]
     }
